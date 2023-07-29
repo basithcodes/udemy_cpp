608160ef8909f2139d2e8ed92c62399ee84c5ed2
Abdul Basith <abdulbasith@pop-os.localdomain>
2023-07-29 22:07:44 +0530

    project config file added

---
+++ configure_files/include/config.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <cstdint>
+#include <string_view>
+
+static constexpr std::string_view project_name = "udemy_cpp";
+static constexpr std::string_view project_version = "0.0.1";
+
+static constexpr std::int32_t project_version_major{0};
+static constexpr std::int32_t project_version_minor{0};
+static constexpr std::int32_t project_version_patch{1};
+
