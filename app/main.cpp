69fca50cae3bc06910989d6df774e628d6f9fedb
Abdul Basith <abdulbasith@pop-os.localdomain>
2023-07-29 14:12:23 +0530

    cmake now works

--- app/main.cpp
+++ app/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include "my_lib.h"
 
 int main (int argc, char *argv[]) {
 	std::cout << "message" << std::endl;
+	print_hello_world();
 	return 0;
 }
 
--- src/my_lib.cpp
+++ src/my_lib.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+#include "my_lib.h"
 void print_hello_world() {
-	std::cout << "Hello World";
+	std::cout << "Hello World" << std::endl;
 }
