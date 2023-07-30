0f33db2f26be0000b672268555d409175e67c631
Abdul Basith <abdulbasith@pop-os.localdomain>
2023-07-30 11:03:18 +0530

    git submodule function working

--- cmake/add_git_submodule.cmake
+++ cmake/add_git_submodule.cmake
@@ -7,16 +7,16 @@
 function(add_git_submodule_func dir)
 	find_package(Git REQUIRED)
 
-	if (NOT EXISTS ${dir}/CMakeLists.txt)
+	if (NOT EXISTS ${PROJECT_SOURCE_DIR}/external_libraries/${dir}/CMakeLists.txt)
 		execute_process(
 			COMMAND ${GIT_EXECUTABLE} 
 			submodule update --init --recursive 
-			-- ${dir}/
+			-- ${PROJECT_SOURCE_DIR}/external_libraries/${dir}
 			WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
 		)
 	endif()
 
-	if(EXISTS ${dir}/CMakeLists.txt) 
-		add_subdirectory(${PROJECT_SOURCE_DIR}/${dir})
+	if(EXISTS ${PROJECT_SOURCE_DIR}/external_libraries/${dir}/CMakeLists.txt) 
+		add_subdirectory(${PROJECT_SOURCE_DIR}/external_libraries/${dir})
 	endif()
 endfunction(add_git_submodule_func)
