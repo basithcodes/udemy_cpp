5a2bb39c4a672595eebc25e145ef10c94b1682d5
Abdul Basith <abdulbasith@pop-os.localdomain>
2023-08-02 08:00:41 +0530

    minor changes to main and cmakelists

--- app/main.cpp
+++ app/main.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
-#include "my_lib.h"
-#include "config.hpp"
-#include "nlohmann/json.hpp"
+#include <vector>
+
+using namespace std;
 
 int main (int argc, char *argv[]) {
-	std::cout << "message" << std::endl;
-	std::cout << "project_version: " << project_version << std::endl;
-	print_hello_world();
-	std::cout << "json lib version: " << NLOHMANN_JSON_VERSION_MAJOR << std::endl;
+	vector <char> vowel;
+	vowel.push_back('a');
+	cout << vowel.at(0) << endl;
 	return 0;
 }
 
